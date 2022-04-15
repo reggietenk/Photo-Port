366cd400eb872c5e7314b63197dd434d1758ae2a
reggietenk <reggietenkorang@gmail.com>
2022-04-14 22:03:16 -0400

    updated Nav component added Gallery component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Nav from '../src/components/Nav'
 import './App.css';
 import About from '../src/components/About ';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
        <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
